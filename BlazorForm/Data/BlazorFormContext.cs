dce073d82184a82194adf191283a13ab156f760e
Jeroen Eelants <jeroen.eelants.bee@outlook.com>
2019-12-09 01:04:48 +0100

    Add project files.

---
+++ BlazorForm/Data/BlazorFormContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorForm.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorForm.Data
+{
+    public class BlazorFormContext : DbContext
+    {
+        public DbSet<ApplicationForm> ApplicationForms { get; set; }
+    }
+}
---
+++ BlazorForm/Models/ApplicationForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorForm.Models
+{
+    public class ApplicationForm
+    {
+        public int Id { get; set; }
+        public Person Person { get; set; }
+        public Message Message { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreationDateTime { get; set; }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+
+    public class Message
+    {
+        public string Content { get; set; }
+    }
+}
